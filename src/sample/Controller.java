e0c24577f5f872979ebb414e3c2da4e316f71190
Naomi <nhid@elpuig.xeill.cat>
2021-03-17 00:40:56 +0100

    iconito

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,16 +5,17 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Controller {
     public void start(ActionEvent actionEvent) throws IOException {
-//        Button b = (Button) actionEvent.getSource();
 
         Parent root = FXMLLoader.load(getClass().getResource("/sample/fxml/Game.fxml"));
         Stage stage = new Stage();
+        stage.getIcons().add(new Image("/sample/arkanoite_icon.png"));
         Scene scene = new Scene(root);
         stage.setTitle("Arkanoite jugando");
         stage.setScene(scene);
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,19 +4,21 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+
 public class Main extends Application {
+
+    String CSS_PATH = "/sample/css/game.css";
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("fxml/MainMenu.fxml"));
+        primaryStage.getIcons().add(new Image("/sample/arkanoite_icon.png"));
         primaryStage.setTitle("Arkanoite");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
-//        Ball ball = new Ball();
-//        ball.start(primaryStage);
-
     }
 
 
