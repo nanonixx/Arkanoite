af2d0cd7296250674ac9f601ebc586e816e89a52
nanonixx <nhid@elpuig.xeill.net>
2021-03-16 18:28:42 +0100

    HE TOCADO LOS FXML MUY BONITOS

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,30 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.io.IOException;
+
 public class Controller {
+    public void start(ActionEvent actionEvent) throws IOException {
+        Button b = (Button) actionEvent.getSource();
+
+
+
+        Parent root = FXMLLoader.load(getClass().getResource("/sample/fxml/Game.fxml"));
+        Stage stage = new Stage();
+        Scene scene = new Scene(root);
+        stage.setTitle("Arkanoite jugando");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void exit(ActionEvent actionEvent) {
+        System.exit(0);
+    }
 }
